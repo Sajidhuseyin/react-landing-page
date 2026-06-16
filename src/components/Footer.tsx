70d05dc7be0be63b1220901f13b9771263ab8325
Sajid Hussain <sajidhuseyin@gmailcom>
2026-06-16 13:53:36 +0500

    added social media liks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,47 @@
-import { FaFacebook, FaLinkedin, FaGithub } from "react-icons/fa";
+ import {
+  FaFacebook,
+  FaLinkedin,
+  FaGithub
+} from "react-icons/fa";
+
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <footer id="contact" className="footer">
-      <div className="footer-content">
 
-        <div className="footer-brand">
-          <h2>LandingPage</h2>
-          <p>
-            Building modern websites that help
-            businesses grow online.
-          </p>
-        </div>
+      <h2>LandingPage</h2>
 
-        <div className="footer-links">
-          <h3>Quick Links</h3>
-          <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Pricing</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+      <p>Email: sajidhuseyin@gmail.com</p>
 
-        <div className="footer-contact">
-          <div className="social-icons">
-            <FaFacebook />
-            <FaLinkedin />
-            <FaGithub />
-            </div>
-          <h3>Contact</h3>
-          <p>sajidhuseyin@gmail.com</p>
-          <p>+923059564916</p>
-        </div>
+      <div className="social-icons">
+
+        <a
+          href="https://facebook.com/sajidhuseyin1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook />
+        </a>
+
+        <a
+          href="https://linkedin.com/in/sajidhuseyin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/Sajidhuseyin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
 
       </div>
 
-      <div className="footer-bottom">
-        © 2026 SmartTech. All Rights Reserved.
-      </div>
     </footer>
   );
 }
